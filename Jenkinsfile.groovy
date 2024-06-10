41d07de852f68994bf782e2437245217bc3cf8fd
KirillJBee <Jbeework@gmail.com>
2024-06-10 17:20:55 +0300

    add groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,23 @@
+pipeline {
+    agent any
+    
+    parameters {
+        booleanParam(name: 'autoApprove', defaultValue: false, description: 'Automatically run apply after generating plan?')
+        choice(name: 'action', choices: ['apply', 'destroy'], description: 'Select the action to perform')
+    }
+
+    environment {
+        AWS_ACCESS_KEY_ID     = credentials('aws-access-key-id')
+        AWS_SECRET_ACCESS_KEY = credentials('aws-secret-access-key')
+        AWS_DEFAULT_REGION    = 'eu-west-1'
+    }
+
+    stages {
+        stage('Checkout') {
+            steps {
+                git branch: 'main', url: 'https://github.com/KirillJBee/diplomataskbeta.git'
+            }
+        }
+    }
+
+}
