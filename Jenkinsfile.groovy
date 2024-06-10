37d7189cdf110e3f9a7158510d38cef1f2ce4f90
KirillJBee <Jbeework@gmail.com>
2024-06-10 18:50:27 +0300

    fix

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -10,12 +10,13 @@ pipeline {
         AWS_ACCESS_KEY_ID     = credentials('aws-access-key-id')
         AWS_SECRET_ACCESS_KEY = credentials('aws-secret-access-key')
         AWS_DEFAULT_REGION    = 'eu-west-1'
+
     }
 
     stages {
         stage('Checkout') {
             steps {
-                git branch: 'main', url: 'https://github.com/KirillJBee/diplomataskbeta'
+                git branch: 'main', credentialsId: '083f268b-1ab6-4871-a097-baea446e7737', url: 'https://github.com/KirillJBee/diplomataskbeta'
             }
         }
     }
