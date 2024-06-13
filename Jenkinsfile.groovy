ab1e42d85954d4a864f810eb9e8a7d12c754a336
KirillJBee <jbeework@gmail.com>
2024-06-13 17:58:39 +0300

    add to jenkins groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -52,8 +52,7 @@ pipeline {
 
         stage('Apply') {
             steps {
-                //sh 'terraform apply -input=false tfplan'
-                sh 'terraform destroy --auto-approve'
+                sh 'terraform apply -input=false tfplan'
             }
         }
 
