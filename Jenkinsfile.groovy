acfda5dafe499b7dccfcb84d2c6a0deb214b94ad
KirillJBee <jbeework@gmail.com>
2024-06-14 10:19:46 +0300

    fix

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -39,7 +39,7 @@ pipeline {
 
         stage('Terraform init') {
             steps {
-                sh './terraform terrform init'
+                sh 'terraform init'
             }  
         }
 
